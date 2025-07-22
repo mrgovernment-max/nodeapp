114669908daf6c73177f9fbf0c59e25d219f644b
EMMAN <efenteng1@gmail.com>
2025-07-22 20:13:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// app.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Emman!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
